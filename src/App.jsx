c06479574270bb49048d1b46cbf162faf811898e
vijaymahes9080 <vijaymahes9080@users.noreply.github.com>
2026-06-08 07:39:55 +0530

    Configure GitHub Pages and Actions deploy workflow

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { ChatProvider } from './context/ChatContext';
 import AppRoutes from './routes';
 
